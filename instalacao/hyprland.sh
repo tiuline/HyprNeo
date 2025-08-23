db8e990cd8ceee7d34cdb11ca578ba43c18324a2
tiuline <pedroramoscontato@hotmail.com>
2025-08-23 09:42:55 -0300

    [010] - Debungando
    parece que ele não conseguiu instalar os utilitarios, vamos ver o que houve

--- instalacao/hyprland.sh
+++ instalacao/hyprland.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "chamando $BASE_DIR/pacotes/00-utilitarios.sh"
+
 source "$BASE_DIR/pacotes/00-hyprland.sh"
-progresso_instalacao_yay "Instalando dependencias do hyprland..." "${HYPRLAND[@]}"
+yay -S "${HYPRLAND[@]}"
 
 echo "instalando Hyprland"
 yay -S hyprland-git
--- instalacao/utilitarios.sh
+++ instalacao/utilitarios.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "chamando $BASE_DIR/pacotes/01-utilitarios.sh"
 source "$BASE_DIR/pacotes/01-utilitarios.sh"
 progresso_instalacao_pacman "Instalando utilitarios essenciais para o sistema..." "${UTILITARIOS[@]}"
